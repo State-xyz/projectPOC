f2b287eb2fdff42d3630e1ad5d142f3ccea25e1f
phuongdv <phuongdv@sskpi.com>
2023-02-03 15:24:51 +0700

    Phase 2: Display his 3x4 table with fixed header row, fixed detail line under “Hello World”

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
+import ListUserComponent from "./components/list-user";
 
 function App() {
   return (
     <div className="App">
-      <header className='App-header'>
+      <header className="App-header">
         <p>HELLO WORLD</p>
       </header>
+      <br></br>
+      <ListUserComponent/>
     </div>
   );
 }
---
+++ client/src/components/list-user/index.js
@@ -0,0 +1,32 @@
+function ListUserComponent() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>age</th>
+            <th>address</th>
+            <th>phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Phuong</td>
+            <td>20</td>
+            <td>Ha Noi</td>
+            <td>0987888999</td>
+          </tr>
+          <tr>
+            <td>Nam</td>
+            <td>30</td>
+            <td>Tp Ho Chi Minh</td>
+            <td>0988666999</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ListUserComponent;
